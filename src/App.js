b61f2e311f7a148496a6db09136608ffd9776e9d
Rikieri Sartor <riki_eri@hotmail.com>
2019-05-31 08:28:47 -0300

    Adding Global Components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import GlobalStyle from './styles/global';
+
 import Sidebar from './components/Sidebar';
+import Player from './components/Player';
+import Header from './components/Header';
+
+import { Wrapper, Container, Content } from './styles/components';
 
 const App = () => (
-  <>
+  <Wrapper>
     <GlobalStyle />
-    <Sidebar />
-  </>
+    <Container>
+      <Sidebar />
+      <Content>
+        <Header />
+      </Content>
+    </Container>
+    <Player />
+  </Wrapper>
 );
 
 export default App;
--- src/styles/global.js
+++ src/styles/global.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-unused-expressions */
 import { createGlobalStyle } from 'styled-components';
+import 'rc-slider/assets/index.css';
 
 const GlobalStyle = createGlobalStyle`
   * {
